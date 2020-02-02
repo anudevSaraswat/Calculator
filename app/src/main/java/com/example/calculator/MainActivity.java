d3cc4a674b4d0f332c7fca44eb04e15cc8a21820
anudevSaraswat <mwanudev@gmail.com>
2020-02-02 17:13:37 +0530

    added brackets

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Stack<Character> expressionStack;
     private TextView displayTextView;
     private int count;
+    private int openingBracCount;
+    private int closingBracCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +35,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button buttonSeven = findViewById(R.id.buttonSeven);
         Button buttonEight = findViewById(R.id.buttonEight);
         Button buttonNine = findViewById(R.id.buttonNine);
-        Button addButton = findViewById(R.id.addButton);
-        Button subtractButton = findViewById(R.id.subtractButton);
+        Button bracketButton = findViewById(R.id.bracketButton);
+        Button modButton = findViewById(R.id.modButton);
         Button multiplyButton = findViewById(R.id.multiplyButton);
         Button divButton = findViewById(R.id.divideButton);
-        Button modButton = findViewById(R.id.modButton);
+        Button addButton = findViewById(R.id.addButton);
+        Button subtractButton = findViewById(R.id.subtractButton);
         Button equalsButton = findViewById(R.id.equalsButton);
         Button dotButton = findViewById(R.id.dotButton);
         Button clearButton = findViewById(R.id.clearButton);
@@ -53,80 +55,106 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonSeven.setOnClickListener(this);
         buttonEight.setOnClickListener(this);
         buttonNine.setOnClickListener(this);
-        addButton.setOnClickListener(this);
-        subtractButton.setOnClickListener(this);
+        bracketButton.setOnClickListener(this);
+        modButton.setOnClickListener(this);
         multiplyButton.setOnClickListener(this);
         divButton.setOnClickListener(this);
-        modButton.setOnClickListener(this);
-        equalsButton.setOnClickListener(this);
-        dotButton.setOnClickListener(this);
+        addButton.setOnClickListener(this);
+        subtractButton.setOnClickListener(this);
+        //equalsButton.setOnClickListener(this);
+        //dotButton.setOnClickListener(this);
         clearButton.setOnClickListener(this);
-        backspace.setOnClickListener(this);
+        //backspace.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        char numOrOp = ' ';
+        String numOrOp = " ";
 
         switch (v.getId()) {
 
-            case R.id.buttonZero: numOrOp = '0';
+            case R.id.buttonZero:
+                numOrOp = "0";
 
                 break;
 
-            case R.id.buttonOne: numOrOp = '1';
+            case R.id.buttonOne:
+                numOrOp = "1";
 
                 break;
 
-            case R.id.buttonTwo: numOrOp = '2';
+            case R.id.buttonTwo:
+                numOrOp = "2";
 
                 break;
 
-            case R.id.buttonThree: numOrOp = '3';
+            case R.id.buttonThree:
+                numOrOp = "3";
 
                 break;
 
-            case R.id.buttonFour: numOrOp = '4';
+            case R.id.buttonFour:
+                numOrOp = "4";
 
                 break;
 
-            case R.id.buttonFive: numOrOp = '5';
+            case R.id.buttonFive:
+                numOrOp = "5";
 
                 break;
 
-            case R.id.buttonSix: numOrOp = '6';
+            case R.id.buttonSix:
+                numOrOp = "6";
 
                 break;
 
-            case R.id.buttonSeven: numOrOp = '7';
+            case R.id.buttonSeven:
+                numOrOp = "7";
 
                 break;
 
-            case R.id.buttonEight: numOrOp = '8';
+            case R.id.buttonEight:
+                numOrOp = "8";
 
                 break;
 
-            case R.id.buttonNine: numOrOp = '9';
+            case R.id.buttonNine:
+                numOrOp = "9";
 
                 break;
 
-            case R.id.addButton: numOrOp = '+';
+            case R.id.bracketButton:
+                numOrOp = insertBracket();
 
                 break;
 
-            case R.id.subtractButton: numOrOp = '-';
+            case R.id.modButton:
+                count = 0;
+                numOrOp = "%";
 
                 break;
 
-            case R.id.multiplyButton: numOrOp = '*';
+            case R.id.multiplyButton:
+                count = 0;
+                numOrOp = "*";
 
                 break;
 
-            case R.id.divideButton: numOrOp = '/';
+            case R.id.divideButton:
+                count = 0;
+                numOrOp = "/";
 
                 break;
 
-            case R.id.modButton: numOrOp = '%';
+            case R.id.addButton:
+                count = 0;
+                numOrOp = "+";
+
+                break;
+
+            case R.id.subtractButton:
+                count = 0;
+                numOrOp = "-";
 
                 break;
 
@@ -135,12 +163,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.clearButton:
-                displayTextView.setText("");
                 count = 0;
+                displayTextView.setText("");
+                emptyStack();
+                openingBracCount = 0;
+                closingBracCount = 0;
 
                 break;
 
-            case R.id.dotButton: numOrOp = '.';
+            case R.id.dotButton:
+                numOrOp = ".";
 
                 break;
 
@@ -150,10 +182,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (count == 15)
             Toast.makeText(this, "Maximum 15 digits allowed!", Toast.LENGTH_SHORT).show();
 
-        if (!(isOperator(numOrOp) && isOperator(expressionStack.peek())) && !expressionStack.isEmpty()){
-            displayTextView.append(numOrOp+"");
-            addToStack(numOrOp);
+        else {
+            if (numOrOp.length() > 1) {
+                displayTextView.append(numOrOp);
+                addToStack(numOrOp.charAt(0));
+                addToStack(numOrOp.charAt(1));
+            } else {
+                if (!expressionStack.isEmpty()) {
+
+                    if (isNumber(numOrOp.charAt(0)) && isNumber(expressionStack.peek()))
+                        count++;
+
+                    if (isOperator(numOrOp.charAt(0)) && isOperator(expressionStack.peek()))
+                        return;
+
+                    if (isNumber(numOrOp.charAt(0))){
+                        if (expressionStack.peek() == ')'){
+                            displayTextView.append("*" + numOrOp);
+                            addToStack('*');
+                            addToStack(numOrOp.charAt(0));
+                            return;
+                        }
+                    }
+
+                    displayTextView.append(numOrOp + "");
+                    addToStack(numOrOp.charAt(0));
+
+                } else {
+                    displayTextView.append(numOrOp + "");
+                    addToStack(numOrOp.charAt(0));
+                    count++;
+                }
+            }
         }
+
+//        if (!(isOperator(numOrOp) && isOperator(expressionStack.peek())) && !expressionStack.isEmpty()){
+//            displayTextView.append(numOrOp+"");
+//            addToStack(numOrOp);
+//        }
     }
 
     void addToStack(char i) {
@@ -167,45 +233,121 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        return builder.toString();
 //    }
 
-    void clearStack(){
-        expressionStack.removeAllElements();
+    void emptyStack() {
+        expressionStack.empty();
     }
 
-    boolean isOperator(char operator){
+    boolean isOperator(char operator) {
 
         boolean flag;
 
-        switch (operator){
+        switch (operator) {
 
             case '+':
             case '-':
             case '*':
             case '/':
             case '%':
-            case '.': flag = true;
+            case '.':
+                flag = true;
 
                 break;
 
-            default: flag =  false;
+            default:
+                flag = false;
         }
 
         return flag;
     }
 
-    void solveExpression(){
+    boolean isNumber(char operator) {
 
-        Stack<String> numberStack = new Stack<>();
-        Stack<Character> operatorStack = new Stack<>();
+        boolean flag;
 
-        for (int i = 0; i < expressionStack.size(); i++){
-            if (isOperator(expressionStack.get(i)))
-                operatorStack.push(expressionStack.get(i));
-            //else
+        switch (operator) {
 
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                flag = true;
+
+                break;
+
+            default:
+                flag = false;
         }
 
-
-
+        return flag;
     }
+
+    String insertBracket() {
+
+        String bracket = null;
+
+        if (expressionStack.isEmpty()) {
+            bracket = "(";
+            openingBracCount++;
+        } else {
+
+            if (expressionStack.contains('(')) {
+
+                if (expressionStack.peek() == '(') {
+                    bracket = "(";
+                    openingBracCount++;
+                } else {
+
+                    if (isOperator(expressionStack.peek())) {
+                        bracket = "(";
+                        openingBracCount++;
+                    }
+
+                    if (expressionStack.peek() == ')') {
+                        if (openingBracCount == closingBracCount) {
+                            bracket = "*(";
+                            openingBracCount++;
+                        } else {
+                            bracket = ")";
+                            closingBracCount++;
+                        }
+                    }
+
+                    if (isNumber(expressionStack.peek())) {
+                        if (openingBracCount == closingBracCount) {
+                            bracket = "*(";
+                            openingBracCount++;
+                        } else {
+                            bracket = ")";
+                            closingBracCount++;
+                        }
+                    }
+                }
+            } else {
+                bracket = "(";
+                openingBracCount++;
+            }
+        }
+
+        return bracket;
+    }
+
+//    void solveExpression() {
+//
+//        Stack<String> numberStack = new Stack<>();
+//        Stack<Character> operatorStack = new Stack<>();
+//
+//        for (int i = 0; i < expressionStack.size(); i++) {
+//            if (isOperator(expressionStack.get(i)))
+//                operatorStack.push(expressionStack.get(i));
+//            else
+//
+//        }
+//    }
 
 }
